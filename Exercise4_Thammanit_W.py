437ba0a4469db7d7dcc4725ca5820059c54b54b4
ThammanitW <96606926+ThammanitW@users.noreply.github.com>
2021-12-27 11:53:38 +0700

    Exercise4

---
+++ Exercise4_Thammanit_W.py
@@ -0,0 +1,9 @@
+fb = 80
+gb = 50
+cs = 75
+cp = 77
+print("---------Your Score---------")
+print("Foundation English : ", fb)
+print("General Business : ", gb)
+print("Introduction to Computer Systems : ", cs)
+print("Computer Programming : ", cp)
