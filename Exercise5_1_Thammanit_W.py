f0ee790fca781e3ae915aa58447a64aec6dd84f5
ThammanitW <96606926+ThammanitW@users.noreply.github.com>
2021-12-27 22:13:19 +0700

    Exercise5.1

---
+++ Exercise5_1_Thammanit_W.py
@@ -0,0 +1,6 @@
+a=int(input("Number 1 : "))
+b=int(input("Number 2 : "))
+print(str(a), "+", str(b), "=", a+b)
+print(str(a), "-", str(b), "=", a-b)
+print(str(a), "*", str(b), "=", a*b)
+print(str(a), "/", str(b), "=", int(a/b))
