17b8ae117bae84d4a5f2d119f98b4cad4c72386c
ThammanitW <96606926+ThammanitW@users.noreply.github.com>
2021-12-30 09:23:36 +0700

    Lecture50

---
+++ Lecture50_Thammanit_W.py
@@ -0,0 +1,17 @@
+x = int (input ("Fisrt Number : "))
+y = int (input ("Second Number : "))
+def addNumber (x,y):
+    print (x+y)
+addNumber(x,y)
+
+def minusNumber (x,y):
+    print (x-y)
+minusNumber(x,y)
+
+def multiplyNumber (x,y):
+    print (x*y)
+multiplyNumber(x,y)
+
+def divideNumber (x,y):
+    print (x/y)
+divideNumber(x,y)
